771f4300edd2880cd081d8eb80d04a6f4d0f97ec
ben9964 <35692789+ben9964@users.noreply.github.com>
2024-01-16 19:04:44 +0000

    begin markov method training

--- ben_project/markov/trainingmarkov.py
+++ ben_project/markov/trainingmarkov.py
@@ -2,12 +2,24 @@
 
 from pathlib import Path
 
-from ..schempy import Schematic
+from schempy import Schematic
 
 def train():
     # TODO: Training Logic Here
     path = Path('schematics/traininghills.schem')
     schem = Schematic.from_file(path)
-    # Loop over all 
+    dict = {}
+
+    for i in range(0, schem.width):
+        for j in range(0, schem.height):
+            for k in range(0, schem.length):
+                block = schem.get_block(i,j,k)
+                # substring after minecraft:
+                material = block.id[10:]
+                if material in dict:
+                    dict[material] += 1
+                else:
+                    dict[material] = 1
+    print(dict["diamond_block"])
 
 
